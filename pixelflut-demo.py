f80d09c073d31f1df4e90bbbb1afb9b92d36a32e
Disler Colin, INI-DOS-FDN-ENB <Colin.Disler1@swisscom.com>
2019-12-29 00:22:08 +0100

    Added offset

--- pixelflut-demo.py
+++ pixelflut-demo.py
@@ -2,14 +2,19 @@ import sys
 import socket
 from PIL import Image
 
+xoffset, yoffset = 100, 100
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((sys.argv[1], int(sys.argv[2])))
+sock.send('OFFSET %d %d\n' % (xoffset, yoffset))
+
 
 def pixel(x,y,r,g,b,a=255):
   if a == 255:
     sock.send('PX %d %d %02x%02x%02x\n' % (x,y,r,g,b))
   else:
     sock.send('PX %d %d %02x%02x%02x%02x\n' % (x,y,r,g,b,a))
+
 
 im = Image.open(sys.argv[3]).convert('RGBA')
 _,_,w,h = im.getbbox()
